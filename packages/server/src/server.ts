2560aa36c1903035af13fce20e28e55df9bc85d8
vinics <vinics.dr@gmail.com>
2022-04-01 18:54:23 -0300

    feature: Init comunication with Google API

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,7 +13,21 @@ app.post('/results', (req, res) => {
 
   const { origem, destino, distancia } = req.body;
 
-  
+  console.log(`Origem: ${origem}`);
+  console.log(`destino: ${destino}`);
+  console.log(`distância: ${distancia}`);
+
+  const api_key = 'use an google API';
+  axios.get(`https://maps.googleapis.com/maps/api/directions/json?origin=${origem}&destination=${destino}&key=${api_key}`)
+        .then(data => {
+          console.dir(data.data);
+        })
+        .catch(e => {
+          console.log(e);
+        })
+
+
+  res.status(200).end();
 });
 
 app.listen(PORT, () => {
