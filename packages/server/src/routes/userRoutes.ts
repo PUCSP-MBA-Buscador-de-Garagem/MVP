c6b1c5bd886217cda3ef299bb038ed929bbd211e
vinics <vinics.dr@gmail.com>
2022-07-15 22:53:29 -0300

    Fix: Route for assinging vehicle to User

--- packages/server/src/routes/userRoutes.ts
+++ packages/server/src/routes/userRoutes.ts
@@ -3,15 +3,18 @@ import { Router } from "express";
 import UserController from '../controllers/UserController';
 import ProviderController from '../controllers/ProviderController';
 import ensureAuthenticatation from '../middlewares/ensureAuthentication';
+import VehiclesController from "../controllers/VehiclesController";
 
 const userRoutes = Router();
 const providerController = new ProviderController();
 const userController = new UserController();
+const vehiclesController = new VehiclesController();
 
 userRoutes.post('/register', userController.create);
 
 userRoutes.use(ensureAuthenticatation);
 userRoutes.patch('/profile', userController.update)
 userRoutes.post('/provider', providerController.create);
+userRoutes.patch('/vehicle', vehiclesController.update)
 
 export default userRoutes;
